923f7b554ca9dd83ebe7b45cc517e368eedd6ce2
Michael McSorley <mcsorlem@kean.edu>
2025-02-11 12:30:45 -0500

    Delete OOP/test/test_addition.py

--- OOP/test/test_addition.py
+++
@@ -1,9 +0,0 @@
-#from app.addition import add
-
-def test_addition():
-    pass
-    #result = add(val1: 2, val2:2)
-    #if result == 4
-     #   pass
-    #result = add(2,2)
-    #assert result ==4
